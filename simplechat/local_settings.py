861dcd7cc7cc9e87fa18df5b82148ae4aa74e7f2
Alexandr Golubev <erwerthg@gmail.com>
2013-12-10 03:00:03 +0400

    modified:   simplechat/local_settings.py

--- simplechat/local_settings.py
+++ simplechat/local_settings.py
@@ -4,8 +4,8 @@ DEV_SERVER = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'chat',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -15,4 +15,4 @@ DATABASES = {
 LOCAL_INSTALLED_APPS = (
 )
 
-MEDIA_ROOT = '/var/yuck/welcomemedia/simple-chat/simple-chat/media/'
+MEDIA_ROOT = ''
